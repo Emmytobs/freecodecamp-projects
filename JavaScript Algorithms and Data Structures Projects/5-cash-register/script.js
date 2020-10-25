eff418634876fe1b9f7f5bae069cd8e22c29e4b5
Emmytobs <eotobo11@gmail.com>
2020-10-25 22:35:52 +0100

    Renamed some variables; refactored the unit_amount function into an object

--- JavaScript Algorithms and Data Structures Projects/5-cash-register/script.js
+++ JavaScript Algorithms and Data Structures Projects/5-cash-register/script.js
@@ -11,41 +11,16 @@ function checkCashRegister(price, cash, cid) {
         return units.split('_')
     };
 
-    function getCurrencyUnitAmount(unit) {
-        let amount;
-        unit = unit.toUpperCase();
-        switch(unit) {
-            case "PENNY":
-                amount = 0.01
-                break;
-            case "NICKEL":
-                amount = 0.05
-                break;
-            case "DIME":
-                amount = 0.1
-                break;
-            case "QUARTER":
-                amount = 0.25
-                break;
-            case "ONE":
-                amount = 1
-                break;
-            case "FIVE":
-                amount = 5
-                break;
-            case "TEN":
-                amount = 10;
-                break;
-            case "TWENTY":
-                amount = 20;
-                break;
-            case "ONE HUNDRED":
-                amount = 100;
-                break;
-            default:
-                amount = null;
-        }
-        return amount;
+    const UNIT_AMOUNT = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.1,
+        "QUARTER":  0.2,
+        "ONE":  1,
+        "FIVE":  5,
+        "TEN":  10,
+        "TWENTY":  20,
+        "ONE HUNDRED": 100,
     }
 
     // Set the currency unit to start and end with when searching for change
@@ -76,7 +51,7 @@ function checkCashRegister(price, cash, cid) {
         for(let cidIndex = 0; cidIndex < cid.length; cidIndex++) {
             let [unit, quantity] = cid[cidIndex];
             if(unit == searchableUnits[idx].toUpperCase()) {
-                let currencyUnitAmount = getCurrencyUnitAmount(unit)
+                let currencyUnitAmount = UNIT_AMOUNT[unit]
                 if(!currencyUnitAmount) {
                     console.log("No amount exists for the unit ", unit);
                     break;
@@ -85,15 +60,15 @@ function checkCashRegister(price, cash, cid) {
                 let numberOfTimesAmountToReturnCanDivideChange = Math.floor(change / currencyUnitAmount);
                 
                 // Calculate the amount of change to return based on the currencyUnitAmount to the number of times it can be taken from the change
-                let possibleChangeToReturn = (currencyUnitAmount * numberOfTimesAmountToReturnCanDivideChange);
+                let changeToGive = (currencyUnitAmount * numberOfTimesAmountToReturnCanDivideChange);
 
-                if(possibleChangeToReturn > quantity || numberOfTimesAmountToReturnCanDivideChange < 1){
+                if(changeToGive > quantity || numberOfTimesAmountToReturnCanDivideChange < 1){
                     continue;
                 }
 
-                change -= possibleChangeToReturn
+                change -= changeToGive
                 
-                changeToReturn.change.push([unit, possibleChangeToReturn])
+                changeToReturn.change.push([unit, changeToGive])
                 changeToReturn.status = "OPEN"
             }
         }
@@ -105,4 +80,4 @@ function checkCashRegister(price, cash, cid) {
 const result = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
 
 console.log(result)
-// Shows insufficient funds; Fix this
+// Shows insufficient funds, instead of OPEN; Fix this
